47820ef6512075964f3f115548df993f13bd9057
zhangwei217245 <zhangwei217245@gmail.com>
2016-03-01 21:38:37 -0600

    the previous AI package was bullshit

--- src/squeezeboard/controller/ai/impl/attack/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * 
- * all the implementations of AttackScoreEvaluator is under this package.
- * Please give each of them a notable name.
- */
-package squeezeboard.controller.ai.impl.attack;
--- src/squeezeboard/controller/ai/impl/risk/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * all the implementations of RiskScoreEvaluator is under this package.
- * Please give each of them a notable name.
- */
-package squeezeboard.controller.ai.impl.risk;
